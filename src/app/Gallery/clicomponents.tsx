b8b7b6a87238a8cbf603294b6dd95f6494b9308f
titan-rocky <rockalbert466@gmail.com>
2023-04-27 22:17:19 +0530

    Sorting with sort()

--- src/app/Gallery/clicomponents.tsx
+++ src/app/Gallery/clicomponents.tsx
@@ -3,6 +3,31 @@ import Image from "next/image";
 import { useState } from "react";
 import React from "react";
 
+type cloudimage = {
+  asset_id: string;
+  public_id: string;
+  folder: string;
+  filename: string;
+  format: string;
+  version: string;
+  created_at: string;
+  uploaded_at: string;
+  bytes: number;
+  backup_bytes: number;
+  width: number;
+  height: number;
+  aspect_ratio: number;
+  pixels: number;
+  url: string;
+  secure_url: string;
+  status: string;
+  access_mode: string;
+  access_control: string;
+  etag: string;
+  created_by: any;
+  uploaded_by: any;
+};
+
 export function Imagecomp(props: { src: any; index: number; onCheck: any }) {
   const [checked, setCheck] = useState(0);
   return (
@@ -15,18 +40,22 @@ export function Imagecomp(props: { src: any; index: number; onCheck: any }) {
         console.log("Hello");
       }}
     >
-      <img
+      <Image
         src={props.src.url}
         alt={`Image ${props.src.filename}`}
         key={props.index}
         width="160"
         height="90"
+        quality="30"
         className="m-1 w-[6.4em] h-[3.6em] border-black border-2 bg-white rounded-lg object-cover 
                             transition-all delay-0 peer-checked/select:w-[4.8em] peer-checked/select:h-[2.7em] 
                             hover:w-[16em] hover:h-[9em] hover:transition-all hover:ease-in-out hover:delay-[600ms] hover:duration-[400ms]"
         loading="eager"
       />
-      <div className="truncate text-black ml-2 w-[80%]" key={props.index}>
+      <div
+        className="truncate text-black ml-2 w-[80%] flex justify-center"
+        key={props.index}
+      >
         <a
           className="text-xs"
           href={props.src.url}
@@ -84,32 +113,71 @@ function SelectButtons(props: { onSelect: any; onDeselect: any }) {
     <div className="p-3  flex justify-center">
       <input
         type="button"
-        className="px-3 py-1 text-md bg-green hover:bg-black border-4 border-black shadow-black shadow-md m-2 text-white"
+        className="px-3 py-1 text-md bg-green hover:bg-black border-4 border-black shadow-black shadow-md mx-2 text-white"
         value="Select All"
         onClick={props.onSelect}
       />
       <input
         type="button"
         value="Deselect All"
-        className="px-3 py-1 text-md bg-red hover:bg-black border-4 border-black shadow-black shadow-md m-2 text-white"
+        className="px-3 py-1 text-md bg-red hover:bg-black border-4 border-black shadow-black shadow-md mx-2 text-white"
         onClick={props.onDeselect}
       />
     </div>
   );
 }
 
-export function Imagelist(props: { images: Array<any> }) {
+export function Imagelist(props: { images: any }) {
+  const [sortby, SetSort] = useState("1");
+  // 1-date , 2-name , 3-type
   const [scount, Setcount] = useState(0);
-  const [imgcomponents, Addurl] = useState(props.images);
+  const [imgcomponents, Setimages] = useState(props.images.resources);
   return (
     <div className="flex flex-col items-stretch">
       <div className="border-4 border-black bg-white rounded-xl flex items-center justify-around text-xl m-5 px-10">
         <div className="">
           <span>Sort By</span>
-          <select className="m-4 text-sm text-white bg-bl font-start2p bd-gray py-2 px-3 shadow-md shadow-black border-2 border-black">
-            <option className="m-3 mt-5 bg-white p-3 text-black">Date</option>
-            <option className="m-3 mt-5 bg-white p-3 text-black">Name</option>
-            <option className="m-3 mt-5 bg-white p-3 text-black">Type</option>
+          <select
+            className="m-4 text-sm text-white bg-bl font-start2p bd-gray py-2 px-3 shadow-md shadow-black border-2 border-black"
+            onChange={(event) => {
+              console.log(event.target.value);
+              if (event.target.value == "1") {
+                Setimages((s: any) => {
+                  s.sort((a: cloudimage, b: cloudimage) => {
+                    return new Date(a.created_at).getTime() <
+                      new Date(b.created_at).getTime()
+                      ? 1
+                      : -1;
+                  });
+                  return s;
+                });
+              } else if (event.target.value == "2") {
+                Setimages((s: any) => {
+                  s.sort((a: cloudimage, b: cloudimage) =>
+                    a.filename > b.filename ? 1 : -1
+                  );
+                  return s;
+                });
+              } else if (event.target.value == "3") {
+                Setimages((s: any) => {
+                  s.sort((a: cloudimage, b: cloudimage) =>
+                    a.format > b.format ? 1 : -1
+                  );
+                  return s;
+                });
+              }
+              SetSort(event.target.value);
+            }}
+          >
+            <option className="m-3 mt-5 bg-white p-3 text-black" value={1}>
+              Date
+            </option>
+            <option className="m-3 mt-5 bg-white p-3 text-black" value={2}>
+              Name
+            </option>
+            <option className="m-3 mt-5 bg-white p-3 text-black" value={3}>
+              Type
+            </option>
           </select>
         </div>
         <div>
@@ -129,6 +197,13 @@ export function Imagelist(props: { images: Array<any> }) {
       </div>
 
       <div className="border-4 border-black rounded-xl m-5 my-2 bg-white flex flex-col flex-wrap items-center bg-opacity-80 p-10 overflow-y-scroll">
+        <div className="flex justify-between items-center w-full">
+          <span>{`${scount} files selected`}</span>
+          <SelectButtons
+            onSelect={() => alert("FUck")}
+            onDeselect={() => alert("Suck")}
+          />
+        </div>
         <div className="grid p-3 grid-cols-6 ms-center">
           {imgcomponents.map((src: any, index: number) => {
             //const regex = new RegExp("([^/]+)$");
@@ -145,12 +220,7 @@ export function Imagelist(props: { images: Array<any> }) {
             );
           })}
         </div>
-        <span>{`${scount} files selected`}</span>
       </div>
-      <SelectButtons
-        onSelect={() => alert("FUck")}
-        onDeselect={() => alert("Suck")}
-      />
     </div>
   );
 }
--- src/app/Gallery/page.tsx
+++ src/app/Gallery/page.tsx
@@ -1,9 +1,9 @@
 import { WhHeader, Navspan } from "../Components/comps";
 import Image from "next/image";
 import "../globals.css";
-import { ReadPath } from "./functions";
-import { Imagecomp, Imagelist } from "./clicomponents";
+import { Imagelist } from "./clicomponents";
 import "process";
+
 import { v2 as Cloudinary } from "cloudinary";
 
 // Configuration
@@ -14,49 +14,28 @@ Cloudinary.config({
   secure: true,
 });
 
-function Butt(props: { name: string }) {
-  return (
-    <input
-      type="button"
-      value={props.name}
-      className={`text-sm text-white bg-bl font-start2p bd-gray py-2 px-3 shadow-md shadow-black m-5 border-2 border-black
-                            hover:bg-black`}
-    />
-  );
+async function fetchThumbUrl(name: string) {
+  const url = Cloudinary.url(name, {
+    width: 160,
+    height: 90,
+    Crop: "fill",
+  });
+  return url;
 }
 
-function Pathtextbox() {
-  return (
-    <input
-      type="text"
-      placeholder={""}
-      className="m-3 px-3 py-2 text-md bg-white text-bl truncate active:decoration-none active:border-0 focus:border-0 placeholder:text-lred rounded-lg "
-    />
-  );
+async function fetch() {
+  let images = { resources: Array<any> };
+  await Cloudinary.search
+    .expression("resource_type:image")
+    .execute()
+    .then((result) => {
+      images = result;
+    });
+  return images;
 }
 
-/*
-function Perdaylist(props: { date: string }) {
-  const images = ReadPath("C:/Users/rocka/Pictures/wallpapers");
-  return (
-    <div className="p-10 flex flex-col">
-      <span className="text-green m-2 text-l">{props.date}</span>
-      <div className="grid p-3 grid-cols-6 ms-center">
-        {images.map((src: any, index: number) => {
-          const regex = new RegExp("([^/]+)$");
-          const m: RegExpExecArray = regex.exec(src.default.src)!;
-          return (
-            <Imagecomp src={src} date={props.date} index={index} path={m} />
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-*/
-
-async function Gallery(props) {
-  console.log(props);
+export default async function Gallery() {
+  const img = await fetch();
   return (
     <body className="bg-gray wafall">
       <WhHeader />
@@ -67,10 +46,8 @@ async function Gallery(props) {
         <Navspan link="Preferences/" value="Preferences" />
       </nav>
       <main className="flex flex-col">
-        <Imagelist images={images.resources} />
+        <Imagelist images={img} />
       </main>
     </body>
   );
 }
-
-export default Gallery;
